5e2950bff0050bd79687a99594e286d767a40786
nbhansali06@gmail.com <nbhansali06@gmail.com>
2025-02-03 23:45:17 +0530

    converting it to a server side component, still doesnt work

--- components/project-posts.tsx
+++ components/project-posts.tsx
@@ -1,7 +1,6 @@
 'use server'
 
 
-import { useState, useEffect } from "react";
 import ProjectShowcaseVertical from "./project-showcase-verticle";
 import { fetchBlogs } from "@/lib/notion";
 // import { defaultDomains } from "./data/default-domains";
@@ -23,41 +22,22 @@ const DEFAULT_PROJECTS = [
   // Add more sample projects as needed...
 ];
 
-const ProjectPosts = () => {
-  const [blogs, setBlogs] = useState<any | null>(null);
-  //   const [files, setFiles] = useState(defaultDomains);
+const ProjectPosts = async () => {
+  try {
+    // const blogs = await fetchBlogs();
+    const blogs = DEFAULT_PROJECTS;
+    const formattedBlogs = blogs.map((blog) => ({
+      name: blog.name,
+      slug: blog.slug,
+      body: blog.body,
+      image: blog.image,
+    }));
 
-  useEffect(() => {
-    const getBlogs = async () => {
-      try {
-        const blogsData = await fetchBlogs();
-        if (blogsData) {
-          const formattedBlogs = blogsData.map((blog: any) => ({
-            name: blog.title,
-            slug: blog.slug,
-            body: blog.subtitle,
-            img: blog.cover,
-          }));
-          // setFiles(formattedPosts.slice(0, 10));
-          setBlogs(formattedBlogs);
-        }
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      } catch (error) {
-        // If there's an error fetching from Notion, use default data
-        console.log("Using default projects data");
-        setBlogs(DEFAULT_PROJECTS);
-      }
-    };
-
-    getBlogs();
-  }, []);
-
-  // Show default projects while loading or if blogs is null
-  if (!blogs) {
+    return <ProjectShowcaseVertical projects={formattedBlogs} />;
+  } catch (error) {
+    console.error("Error fetching blogs:", error);
     return <ProjectShowcaseVertical projects={DEFAULT_PROJECTS} />;
   }
-
-  return <ProjectShowcaseVertical projects={blogs} />;
 };
 
 export default ProjectPosts;
